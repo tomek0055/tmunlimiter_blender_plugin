c6da4c46947b43e8e8b25975a9b6d84edbb55a9e
tomek0055 <kemot00055@gmail.com>
2022-12-26 10:36:52 +0100

    Update validators

--- src/validators.py
+++ src/validators.py
@@ -1,19 +1,19 @@
 import bpy
 
-def validate_plug_visual_3d( object : bpy.types.Object ) -> bool :
+def validate_plug_visual_3d( object: bpy.types.Object ) -> bool :
     return object.type == "MESH" and len( object.data.polygons ) > 0
 
-def validate_plug_surface( object : bpy.types.Object ) -> bool :
+def validate_plug_surface( object: bpy.types.Object ) -> bool :
     return \
         validate_plug_visual_3d( object ) \
             and \
         len(
             list(
                 filter(
-                    lambda material : \
+                    lambda material: \
                         material is not None \
                             and \
-                        material.unlimiter_data.collision_type != "NO_COLLISION",
+                        material.unlimiter_material_collision.is_collidable,
 
                     object.data.materials
                 )

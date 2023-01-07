746be530b57347952a4410c2c4157e2bcdf4951c
tomek0055 <kemot00055@gmail.com>
2023-01-07 21:18:16 +0100

    Remove export_solid_gbx from mainline branch

--- src/__init__.py
+++ src/__init__.py
@@ -14,18 +14,11 @@ from .export_block_gbx import (
     __unregister__ as export_block_gbx_unregister,
 )
 
-from .export_solid_gbx import (
-    __register__ as export_solid_gbx_register,
-    __unregister__ as export_solid_gbx_unregister,
-)
-
 def register() :
     export_block_gbx_register()
-    export_solid_gbx_register()
 
 def unregister() :
     export_block_gbx_unregister()
-    export_solid_gbx_unregister()
 
 if __name__ == "__main__" :
     register()

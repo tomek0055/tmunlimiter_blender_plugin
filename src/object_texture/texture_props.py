8bc4a027cc1a4529a42b00888a3f158ece30ae26
tomek0055 <kemot00055@gmail.com>
2023-02-17 14:13:21 +0100

    Add texture properties

---
+++ src/object_texture/texture_props.py
@@ -0,0 +1,58 @@
+import bpy
+
+class TMUnlimiterTextureProps( bpy.types.PropertyGroup ) :
+
+    filepath: bpy.props.StringProperty(
+        name = "File path",
+        description = "Texture file path relative to the \"Texures\" directory"
+    )
+
+    filtering: bpy.props.EnumProperty(
+        name = "Texture filtering",
+        items = [
+            ( "0", "Point", "Point" ),
+            ( "1", "Bilinear", "Bilinear" ),
+            ( "2", "Trilinear", "Trilinear" ),
+            ( "3", "Anisotropic", "Anisotropic" ),
+        ],
+        default = "3",
+    )
+
+    addressing: bpy.props.EnumProperty(
+        name = "Texture addressing",
+        items = [
+            ( "0", "Wrap", "Wrap" ),
+            ( "1", "Mirror", "Mirror" ),
+            ( "2", "Clamp", "Clamp" ),
+            ( "3", "BorderSM3", "BorderSM3" ),
+        ],
+        default = "2",
+    )
+
+    def get_texture_type( self ) -> str :
+        raise Exception( "Not implemented" )
+
+class TMUnlimiterDiffuseProps( TMUnlimiterTextureProps ) :
+
+    def get_texture_type( self ) -> str :
+        return "Diffuse"
+
+class TMUnlimiterSpecularProps( TMUnlimiterTextureProps ) :
+
+    def get_texture_type( self ) -> str :
+        return "Specular"
+
+class TMUnlimiterNormalProps( TMUnlimiterTextureProps ) :
+
+    def get_texture_type( self ) -> str :
+        return "Normal"
+
+class TMUnlimiterLightingProps( TMUnlimiterTextureProps ) :
+    
+    def get_texture_type(self) -> str :
+        return "Lighting"
+
+class TMUnlimiterOcclusionProps( TMUnlimiterTextureProps ) :
+
+    def get_texture_type( self ) -> str :
+        return "Occlusion"

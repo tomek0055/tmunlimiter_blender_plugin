320d533726db0facacdf2d20025bcc2f0dbe24e3
tomek0055 <kemot00055@gmail.com>
2023-02-17 14:12:03 +0100

    Update custom block archivization

--- src/unlimiter_block.py
+++ src/unlimiter_block.py
@@ -12,42 +12,51 @@ def unlimiter_block(
     block_id: str,
     block_author: str,
     spawn_point: list[float],
-    root: bpy.types.Collection
+    root: bpy.types.Collection | bpy.types.Object
 ) :
     header_chunk = HeaderChunk( CLASS_ID )
-    header_chunk.nat8( VERSION )
     header_chunk.mw_id( block_id )
     header_chunk.mw_id( block_author )
     header_chunk.nat64( int( datetime.now( timezone.utc ).timestamp() * 1000 ) )
     gbx.header_chunk( header_chunk )
 
-    gbx.nat32( CLASS_ID )
-    gbx.nat8( VERSION )
+    if "replacement_textures" not in gbx.context :
+        gbx.context[ "replacement_textures" ] = {}
 
-    if "custom_texture_refs" not in gbx.context :
-        gbx.context[ "custom_texture_refs" ] = {}
-
-    new_body = BytesIO()
+    if "custom_texture_references" not in gbx.context :
+        gbx.context[ "custom_texture_references" ] = {}
 
     # solid
-    old_body = gbx.attach_buffer( new_body )
     plug_solid( gbx, root )
-    gbx.attach_buffer( old_body )
+    solid_data = gbx.attach_buffer( BytesIO() )
+
+    # replacement textures
+    replacement_textures = gbx.context[ "replacement_textures" ]
+    gbx.nat32( len( replacement_textures ) )
+
+    for replacement_texture_type in replacement_textures :
+        gbx.nat32( replacement_textures[ replacement_texture_type ] )
+        gbx.nat32( int( replacement_texture_type ) )
 
     # unlimiter custom texture references
-    custom_texture_references = gbx.context[ "custom_texture_refs" ]
+    custom_texture_references = gbx.context[ "custom_texture_references" ]
     gbx.nat32( len( custom_texture_references ) )
 
-    for instance_idx, path in custom_texture_references.values() :
-        gbx.nat32( instance_idx )
-        gbx.string( path )
-        gbx.nat32( 0 ) # adressing
-        gbx.nat32( 0 ) # filtering
+    for custom_texture_tuple in custom_texture_references :
+        gbx.nat32( custom_texture_references[ custom_texture_tuple ] )
+        
+        gbx.string( custom_texture_tuple[ 0 ] ) # texture.filepath
+        gbx.nat32( int( custom_texture_tuple[ 1 ] ) ) # texture.filtering
+        gbx.nat32( int( custom_texture_tuple[ 2 ] ) ) # texture.addressing
 
-    gbx.data( new_body )
-    gbx.mw_id( block_id )
-    gbx.mw_id( block_author )
+    gbx.data( solid_data )
     gbx.real( spawn_point[ 0 ] )
     gbx.real( spawn_point[ 1 ] )
     gbx.real( spawn_point[ 2 ] )
-    gbx.nat32( 0xFACADE01 )
+    block_data = gbx.attach_buffer( BytesIO() )
+
+    gbx.nat32( CLASS_ID + VERSION )
+    gbx.nat32( 0x534b4950 )
+    gbx.nat32( block_data.tell() )
+    gbx.data( block_data )
+    gbx.nat32( 0xfacade01 )

d96bb6b545a2ee85c4664a7d84d8933e531dac12
tomek0055 <kemot00055@gmail.com>
2023-12-26 18:37:56 +0100

    Delete plug_tree_from_collection

--- src/plug_solid.py
+++ src/plug_solid.py
@@ -1,11 +1,8 @@
-from .plug_tree import (
-    plug_tree_from_collection,
-    plug_tree_from_object,
-)
+from .plug_tree import plug_tree_from_object
 from .blender_gbx import GbxArchive
 import bpy
 
-def plug_solid( gbx: GbxArchive, root: bpy.types.Object | bpy.types.Collection ) :
+def plug_solid( gbx: GbxArchive, root: bpy.types.Object ) :
 # 0x09005011 -- start
     gbx.nat32( 0x09005011 )
     gbx.nat32( 0x00000000 )
@@ -13,10 +10,8 @@ def plug_solid( gbx: GbxArchive, root: bpy.types.Object | bpy.types.Collection )
 
     if type( root ) is bpy.types.Object :
         gbx.mw_ref( plug_tree_from_object, root )
-    elif type( root ) is bpy.types.Collection :
-        gbx.mw_ref( plug_tree_from_collection, root )
     else :
-        gbx.nat32( 0xFFFFFFFF )
+        gbx.nat32( 0xffffffff )
 # 0x09005011 -- end
-    gbx.nat32( 0xFACADE01 )
+    gbx.nat32( 0xfacade01 )
 # 0x09005000 -- end
--- src/plug_tree.py
+++ src/plug_tree.py
@@ -9,31 +9,6 @@ from math import pi
 import mathutils
 import bpy
 
-def plug_tree_from_collection( gbx: GbxArchive, collection: bpy.types.Collection ) :
-    gbx.nat32( 0x0904f000 )
-
-    gbx.nat32( 0x0904f00d )
-    gbx.mw_id( collection.name )
-    gbx.nat32( 0xffffffff )
-
-    flags = 0x00004000
-    objects  = list( filter( lambda object: object.parent is None, collection.all_objects ) )
-
-    if len( objects ) :
-        gbx.nat32( 0x0904f006 )
-        gbx.nat32( 0x0000000a )
-        gbx.nat32( len( objects ) )
-
-        for object in objects :
-            was_valid_ref, child_flags, _ = gbx.mw_ref( plug_tree_from_object, object )
-
-            if was_valid_ref :
-                flags |= child_flags & 0xfffffffb
-
-    gbx.nat32( 0x0904f01a )
-    gbx.nat32( flags )
-    gbx.nat32( 0xfacade01 )
-
 def plug_tree_from_object( gbx: GbxArchive, object: bpy.types.Object ) :
     if object.type == "LIGHT" :
         gbx.nat32( 0x09062000 )

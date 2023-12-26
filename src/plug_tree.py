cc76b30fcc253b181f6bb650a348a2d73d6f9066
tomek0055 <kemot00055@gmail.com>
2023-12-26 18:40:03 +0100

    Implement new properties in plug_tree_from_object function

--- src/plug_tree.py
+++ src/plug_tree.py
@@ -9,36 +9,66 @@ from math import pi
 import mathutils
 import bpy
 
-def plug_tree_from_object( gbx: GbxArchive, object: bpy.types.Object ) :
+def _get_plug_tree_class_identifier( object: bpy.types.Object ) :
     if object.type == "LIGHT" :
-        gbx.nat32( 0x09062000 )
-    else :
-        gbx.nat32( 0x0904f000 )
+        light_data: bpy.types.Light = object.data
 
-    gbx.nat32( 0x0904f00d )
-    gbx.mw_id( object.name )
-    gbx.nat32( 0xffffffff )
+        if light_data.type in { "POINT", "SPOT" } :
+            return 0x09_062_000
+
+        raise Exception( f'The selected light type "{ light_data.type }" is not supported by the game' )
+
+    if object.type not in { "MESH", "EMPTY" } :
+        raise Exception( f'The type "{ object.type }" of an object "{ object.name }" is not supported by the game or the exporter' )
+
+    if object.type == "EMPTY" and object.unlimiter_object_settings.is_visual_mip :
+        return 0x09_015_000
+
+    return 0x09_04f_000
+
+def plug_tree_from_object( gbx: GbxArchive, object: bpy.types.Object ) :
+    object_settings: TMUnlimiterObjectSettings = object.unlimiter_object_settings
+
+    plug_tree_class_identifier = _get_plug_tree_class_identifier( object )
+    gbx.nat32( plug_tree_class_identifier )
+
+    if object_settings.can_export_object_name :
+        gbx.nat32( 0x0904f00d )
+        gbx.mw_id( object.name )
+        gbx.nat32( 0xffffffff )
 
     flags = 0x00004004
     scale = object.scale.copy()
-
-    if scale.x != 1.0 or scale.y != 1.0 or scale.z != 1.0 :
-        raise Exception( f'Transform of an object "{ object.name }" has a scale other than 1.0 - Please apply scale transform to your object' )
-
     rotation = object.rotation_euler.copy()
     has_visual = False
     is_collidable = False
 
-    if len( object.children ) :
-        gbx.nat32( 0x0904f006 )
-        gbx.nat32( 0x0000000a )
-        gbx.nat32( len( object.children ) )
+    child_objects = list( filter( lambda object: not object.unlimiter_object_settings.exclude_from_export, object.children ) )
+    child_count = len( child_objects )
 
-        for children in object.children :
-            was_valid_ref, child_flags, _ = gbx.mw_ref( plug_tree_from_object, children )
+    if child_count :
+        if plug_tree_class_identifier == 0x09_015_000 :
+            child_objects.sort( key = lambda object: object.unlimiter_object_settings.visual_mip_distance )
 
-            if was_valid_ref :
-                flags |= child_flags
+            gbx.nat32( 0x09015002 )
+            gbx.nat32( child_count )
+
+            for child_object in child_objects :
+                gbx.real( child_object.unlimiter_object_settings.visual_mip_distance )
+                valid_ref, child_flags, _ = gbx.mw_ref( plug_tree_from_object, child_object )
+
+                if valid_ref :
+                    flags |= child_flags
+        else :
+            gbx.nat32( 0x0904f006 )
+            gbx.nat32( 0x0000000a )
+            gbx.nat32( child_count )
+
+            for child_object in child_objects :
+                valid_ref, child_flags, _ = gbx.mw_ref( plug_tree_from_object, child_object )
+
+                if valid_ref :
+                    flags |= child_flags
 
     if object.type == "LIGHT" :
         light: bpy.types.Light = object.data
@@ -48,14 +78,11 @@ def plug_tree_from_object( gbx: GbxArchive, object: bpy.types.Object ) :
         elif light.type == "SPOT" :
             func = gx_light_spot
             rotation.x += pi / 2
-        else :
-            raise Exception( f'Not supported light type: "{ light.type }"' )
 
         gbx.nat32( 0x09062001 )
         gbx.mw_ref( func, light )
         gbx.nat32( 0xffffffff )
-    else :
-        object_settings: TMUnlimiterObjectSettings = object.unlimiter_object_settings
+    elif object.type == "MESH" :
         gbx.nat32( 0x0904f014 )
 
         if object_settings.can_export_geometry :

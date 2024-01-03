0f094ef17f92986d86b48c8d903b1c6511a06bc5
tomek0055 <kemot00055@gmail.com>
2024-01-03 14:45:46 +0100

    Change plugin version to 1.1.1

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name" : "TrackMania Unlimiter: Custom Block exporter",
     "author": "Tomek0055",
-    "version" : (1, 1, 0),
+    "version" : (1, 1, 1),
     "blender" : (4, 0, 0),
     "doc_url" : "https://github.com/tomek0055/tmunlimiter_blender_plugin",
     "support" : "COMMUNITY",
